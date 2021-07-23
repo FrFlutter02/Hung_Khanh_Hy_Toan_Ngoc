8077cc729c516455a6b3033cc5f53c9e416778af
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-07-23 09:21:29 +0700

    save pubspec.yaml

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -18,6 +18,7 @@ class App extends StatelessWidget {
         "/loginScreen": (context) => LoginScreen(),
         "/signupScreen": (context) => SignupScreen(),
       },
+      initialRoute: '/loginScreen',
     );
     // );
   }
