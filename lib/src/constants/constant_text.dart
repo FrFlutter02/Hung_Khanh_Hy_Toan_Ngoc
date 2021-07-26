23a77adbdbb6d584ae039b9b975364fe56f290f0
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-07-26 10:53:48 +0700

    add class OnboardingTabletText

--- lib/src/constants/constant_text.dart
+++ lib/src/constants/constant_text.dart
@@ -1,5 +1,5 @@
 class AppText {
-  static const String buttonGreen = "scratch";
+  static const String iconText = "scratch";
 }
 
 class LoginScreenText {
@@ -15,11 +15,20 @@ class LoginScreenText {
 
 class SignupScreenText {
   static const String startFromSratch = "Start from Scratch";
-  static const String welcome = "Create account to continue";
+  static const String createAccountToContinue = "Create account to continue";
   static const String fullNameLabel = "Full Name";
   static const String emailNameLabel = "Email";
   static const String passwordLabel = "Password";
   static const String createAccount = "Create Account";
   static const String alreadyHaveAnAccount = "Already have an account?";
   static const String loginHere = "Login Here";
+}
+
+class OnboardingTabletText {
+  static const String firstTitle =
+      "Join over 50 millions people sharing recipes everday";
+  static const String secondTitle =
+      "Never run out ideas again. Try new food, ingredients, cooking style, and more";
+  static const String buttonJoin = "Join Scratch";
+  static const String buttonLearn = "Learn More";
 }
