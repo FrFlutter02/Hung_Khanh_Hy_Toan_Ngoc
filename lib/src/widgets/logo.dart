d5c12cb08d3ac00e8b663b322254661a55fe0262
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-07-26 10:52:17 +0700

    feature: Added logo widget

---
+++ lib/src/widgets/logo.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../constants/constant_colors.dart' as colors;
+import '../constants/constant_text.dart' as text;
+
+class Logo extends StatelessWidget {
+  final double width;
+  final double height;
+
+  const Logo({required this.width, required this.height, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 18,
+                height: 26,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/images/logo_icon.png'))),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                text.AppText.iconText,
+                style: TextStyle(
+                  color: colors.AppColor.iconText,
+                  fontFamily: 'Nunito-Bold',
+                  fontSize: 20,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
