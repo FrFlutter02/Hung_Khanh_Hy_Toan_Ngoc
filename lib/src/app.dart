23d32f5c9f272159cb343c0515d137056baf2319
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-07-26 10:56:13 +0700

    Transparant Statusbar

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../src/screens/login_screen.dart';
@@ -8,12 +9,19 @@ import '../src/screens/signup_screen.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ));
     return
         // MultiBlocProvider(
         //     providers: [],
         //     child:
         MaterialApp(
-      theme: ThemeData(fontFamily: "Nunito-Regular"),
+      theme: ThemeData(
+        fontFamily: "Nunito-Regular",
+      ),
       routes: {
         "/": (context) => OnboardingScreen(),
         "/loginScreen": (context) => LoginScreen(),
