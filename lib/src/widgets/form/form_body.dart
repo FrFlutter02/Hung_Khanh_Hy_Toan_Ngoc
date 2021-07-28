2b11b68d7cd5efddcc4ffda83faa3bb9858791a3
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-07-28 13:35:55 +0700

    added helper file

--- lib/src/widgets/form/form_body.dart
+++ lib/src/widgets/form/form_body.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_device_type/flutter_device_type.dart';
 
 import '../../constants/constant_colors.dart';
 import '../../constants/constant_text.dart';
 import '../custom_button.dart';
+import '../../helper.dart';
 
 class FormBody extends StatefulWidget {
   final List<Widget> textFormFieldList;
@@ -24,41 +26,39 @@ class FormBody extends StatefulWidget {
 
 class _ScratchFormState extends State<FormBody> {
   final _formKey = GlobalKey<FormState>();
-
-  double getHeight(double designedPixel, double screenHeight) {
-    return designedPixel / (widget.isTabletScreen ? 1024 : 812) * screenHeight;
-  }
-
-  double getWidth(double designedPixel, double screenWidth) {
-    return designedPixel / (widget.isTabletScreen ? 768 : 375) * screenWidth;
-  }
+  final Helper _helper = Helper();
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return Container(
         width: MediaQuery.of(context).size.width,
         padding: EdgeInsets.only(
-          left: getWidth(widget.isTabletScreen ? 171 : 25, screenWidth),
-          right: getWidth(widget.isTabletScreen ? 171 : 25, screenWidth),
-          top: getHeight(20, screenHeight),
-          bottom: widget.isTabletScreen ? getHeight(45, screenHeight) : 0,
+          left: _helper.width(widget.isTabletScreen ? 171 : 25),
+          right: _helper.width(widget.isTabletScreen ? 171 : 25),
+          top: _helper.height(20),
+          bottom: widget.isTabletScreen ? _helper.height(45) : 0,
         ),
         child: Form(
           key: _formKey,
-          child: Card(
-            elevation: widget.isTabletScreen ? 5 : 0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(12),
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppColor.white,
+              borderRadius: BorderRadius.all(
+                Radius.circular(8),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: AppColor.secondaryGrey.withOpacity(0.2),
+                  blurRadius: 15,
+                  spreadRadius: 12,
+                  offset: Offset(0, 8),
+                ),
+              ],
             ),
             child: Padding(
               padding: EdgeInsets.symmetric(
-                horizontal:
-                    widget.isTabletScreen ? getWidth(50, screenWidth) : 0,
-                vertical:
-                    widget.isTabletScreen ? getHeight(50, screenWidth) : 0,
+                horizontal: widget.isTabletScreen ? _helper.width(50) : 0,
+                vertical: widget.isTabletScreen ? _helper.height(50) : 0,
               ),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -70,12 +70,12 @@ class _ScratchFormState extends State<FormBody> {
                       fontSize: 14,
                     ),
                   ),
-                  SizedBox(height: getHeight(30, screenHeight)),
+                  SizedBox(height: _helper.height(30)),
                   ...widget.textFormFieldList,
                   CustomButton(
                     enabled: true,
-                    width: screenWidth,
-                    height: getHeight(50, screenHeight),
+                    width: Device.screenWidth,
+                    height: _helper.height(50),
                     handlePressed: () {
                       if (_formKey.currentState!.validate()) {
                         ScaffoldMessenger.of(context).showSnackBar(
