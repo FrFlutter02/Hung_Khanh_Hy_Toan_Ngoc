2d37f21e7ca3bf01db1752b158679f684508ed49
giahy1 <ngiahyiU@gmail.com>
2021-08-02 15:52:05 +0700

    final update

--- lib/src/blocs/forgot_password_bloc/forgot_password_bloc.dart
+++ lib/src/blocs/forgot_password_bloc/forgot_password_bloc.dart
@@ -2,14 +2,11 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile_app/src/constants/constant_text.dart';
-
-import 'package:mobile_app/src/services/user_services.dart';
-import 'package:mobile_app/src/utils/validator.dart';
-
+import '../../constants/constant_text.dart';
+import '../../services/user_services.dart';
+import '../../utils/validator.dart';
 part 'forgot_password_event.dart';
 part 'forgot_password_state.dart';
 
@@ -38,30 +35,6 @@ class ForgotPasswordBloc
           ));
           yield ForgotPasswordSuccess();
         }
-
-        // ScaffoldMessenger.of(event.context).showSnackBar(SnackBar(
-        //   content: const Text(ForgotPasswordText.snackbarContentScuess),
-        // ));
-        // if (event.email != '') {
-        //   if (AppText.emailRegex.hasMatch(event.email)) {
-        //     if (await userServices.userDidExist(event.email)) {
-        //       userServices.updateUser(event.email);
-        //       ScaffoldMessenger.of(event.context).showSnackBar(SnackBar(
-        //         content: const Text(ForgotPasswordText.snackbarContentScuess),
-        //       ));
-
-        //       yield ForgotPassworDoesEmailExist('');
-        //     } else {
-        //       yield ForgotPassworDoesEmailExist(
-        //           ForgotPasswordText.snackbarContentEmailExist);
-        //     }
-        //   } else {
-        //     yield ForgotPassworDoesEmailExist(AppText.emailInvalidErrorText);
-        //   }
-        // } else {
-        //   yield ForgotPassworDoesEmailExist("Please Enter email");
-        // }
-
         break;
       default:
     }
