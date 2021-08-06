12806a930e43b0654624519b1235aee9a0430236
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-08-06 12:16:57 +0700

    Modified LoginAndSignupBody and CustomButton

--- lib/src/widgets/custom_button.dart
+++ lib/src/widgets/custom_button.dart
@@ -1,34 +1,44 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_app/src/blocs/signup_bloc/signup_bloc.dart';
+import 'package:mobile_app/src/blocs/signup_bloc/signup_state.dart';
 
 import '../constants/constant_colors.dart';
 import '../utils/screen_util.dart';
 
-class CustomButton extends StatelessWidget {
-  final bool enabled;
+class CustomButton extends StatefulWidget {
+  final SignupBloc? signupBloc;
   final double width;
   final double height;
   final String value;
-  final bool isLoading;
   final void Function() buttonOnPress;
 
   const CustomButton(
-      {required this.enabled,
+      {required this.signupBloc,
       required this.width,
       required this.height,
       required this.value,
-      required this.isLoading,
       required this.buttonOnPress,
       Key? key})
       : super(key: key);
 
   @override
+  _CustomButtonState createState() => _CustomButtonState();
+}
+
+class _CustomButtonState extends State<CustomButton> {
+  StreamSubscription? signupStreamSubscription;
+  bool isLoading = false;
+
+  @override
   Widget build(BuildContext context) {
     final ScreenUtil _screenUtil = ScreenUtil();
     return Container(
-      width: width,
-      height: height,
+      width: widget.width,
+      height: widget.height,
       child: ElevatedButton(
-        onPressed: enabled ? buttonOnPress : null,
+        onPressed: !isLoading ? widget.buttonOnPress : null,
         child: isLoading
             ? SizedBox(
                 child: CircularProgressIndicator(
@@ -37,7 +47,7 @@ class CustomButton extends StatelessWidget {
                 height: _screenUtil.height(25),
               )
             : Text(
-                value,
+                widget.value,
                 style: Theme.of(context)
                     .textTheme
                     .bodyText2!
@@ -55,5 +65,25 @@ class CustomButton extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    signupStreamSubscription?.cancel();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    signupStreamSubscription = widget.signupBloc?.stream.listen((signupState) {
+      setState(() {
+        if (signupState is SignupInProgress) {
+          isLoading = true;
+        } else {
+          isLoading = false;
+        }
+      });
+    });
+    super.initState();
   }
 }
--- lib/src/widgets/login_and_signup/login_and_signup_body.dart
+++ lib/src/widgets/login_and_signup/login_and_signup_body.dart
@@ -1,15 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_device_type/flutter_device_type.dart';
 
 import '../../blocs/signup_bloc/signup_bloc.dart';
-import '../../blocs/signup_bloc/signup_state.dart';
 import '../../constants/constant_colors.dart';
 import '../../utils/screen_util.dart';
 import '../custom_button.dart';
 
-class LoginAndSignupBody extends StatefulWidget {
+class LoginAndSignupBody extends StatelessWidget {
   final SignupBloc? signupBloc;
   final List<Widget> textFormFieldList;
   final String titleText;
@@ -32,31 +29,22 @@ class LoginAndSignupBody extends StatefulWidget {
       required this.destinationRoute,
       Key? key})
       : super(key: key);
-
-  @override
-  _LoginAndSignupBodyState createState() => _LoginAndSignupBodyState();
-}
-
-class _LoginAndSignupBodyState extends State<LoginAndSignupBody> {
-  final ScreenUtil _screenUtil = ScreenUtil();
-  bool isLoading = false;
-  StreamSubscription? signupStreamSubscription;
-
   @override
   Widget build(BuildContext context) {
+    final ScreenUtil _screenUtil = ScreenUtil();
     return Padding(
       padding: EdgeInsets.only(
-          left: _screenUtil.width(widget.isTabletScreen ? 171 : 25),
-          right: _screenUtil.width(widget.isTabletScreen ? 171 : 25),
-          top: widget.isTabletScreen ? 0 : _screenUtil.height(20),
-          bottom: widget.isTabletScreen ? _screenUtil.height(45) : 0),
+          left: _screenUtil.width(isTabletScreen ? 171 : 25),
+          right: _screenUtil.width(isTabletScreen ? 171 : 25),
+          top: isTabletScreen ? 0 : _screenUtil.height(20),
+          bottom: isTabletScreen ? _screenUtil.height(45) : 0),
       child: Container(
         decoration: BoxDecoration(
           color: AppColor.white,
           borderRadius: BorderRadius.all(
             Radius.circular(8),
           ),
-          boxShadow: widget.isTabletScreen
+          boxShadow: isTabletScreen
               ? [
                   BoxShadow(
                     color: AppColor.secondaryGrey.withOpacity(0.2),
@@ -69,32 +57,31 @@ class _LoginAndSignupBodyState extends State<LoginAndSignupBody> {
         ),
         child: Padding(
           padding: EdgeInsets.symmetric(
-            horizontal: widget.isTabletScreen ? _screenUtil.width(50) : 0,
-            vertical: widget.isTabletScreen ? _screenUtil.height(50) : 0,
+            horizontal: isTabletScreen ? _screenUtil.width(50) : 0,
+            vertical: isTabletScreen ? _screenUtil.height(50) : 0,
           ),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                widget.titleText,
+                titleText,
                 style: Theme.of(context).textTheme.bodyText2!,
               ),
               SizedBox(height: _screenUtil.height(30)),
-              ...widget.textFormFieldList,
+              ...textFormFieldList,
               CustomButton(
-                  enabled: !isLoading,
-                  value: widget.buttonText,
+                  signupBloc: signupBloc,
+                  value: buttonText,
                   width: Device.screenWidth,
                   height: _screenUtil.height(50),
-                  isLoading: isLoading,
-                  buttonOnPress: widget.buttonOnPress),
+                  buttonOnPress: buttonOnPress),
               Container(
                 child: Column(
                   children: [
                     SizedBox(height: _screenUtil.height(30)),
                     Center(
                       child: Text(
-                        widget.bottomTitleText,
+                        bottomTitleText,
                         style: Theme.of(context)
                             .textTheme
                             .bodyText2!
@@ -104,11 +91,12 @@ class _LoginAndSignupBodyState extends State<LoginAndSignupBody> {
                     SizedBox(height: _screenUtil.height(5)),
                     Center(
                       child: InkWell(
+                        key: Key('bottomLink'),
                         onTap: () {
-                          Navigator.pushNamed(context, widget.destinationRoute);
+                          Navigator.pushNamed(context, destinationRoute);
                         },
                         child: Text(
-                          widget.bottomLinkText,
+                          bottomLinkText,
                           style: Theme.of(context)
                               .textTheme
                               .bodyText1!
@@ -126,27 +114,5 @@ class _LoginAndSignupBodyState extends State<LoginAndSignupBody> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    signupStreamSubscription?.cancel();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    signupStreamSubscription = widget.signupBloc?.stream.listen((signupState) {
-      if (signupState is SignupInProgress) {
-        setState(() {
-          isLoading = true;
-        });
-      } else {
-        setState(() {
-          isLoading = false;
-        });
-      }
-    });
-    super.initState();
   }
 }
