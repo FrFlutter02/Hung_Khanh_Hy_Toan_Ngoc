a57e6ec7826b1d93d9aff4377f39971223447d87
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-08-09 00:20:26 +0700

    signup_bloc_test

--- test/bloc_tests/signup_bloc_test.dart
+++ test/bloc_tests/signup_bloc_test.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_test/bloc_test.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/src/blocs/signup_bloc/signup_bloc.dart';
 import 'package:mobile_app/src/blocs/signup_bloc/signup_event.dart';
@@ -11,38 +10,57 @@ import 'package:mockito/mockito.dart';
 
 import '../cloud_firestore_mock.dart';
 
-class MockUserServices extends Mock implements UserServices {}
+class MockUserServices extends Mock implements UserServices {
+  @override
+  Future<UserCredential?> signUp(UserModel userModel) async {
+    if (userModel.fullName != 'valid' &&
+        userModel.email != 'valid' &&
+        userModel.password != 'valid') {
+      throw Exception();
+    }
+  }
+}
+
+// class MockUserModel extends Mock implements UserModel {}
+
+class MockUserCredential extends Mock implements UserCredential {}
 
 void main() {
-  UserServices userServices = MockUserServices();
-  SignupBloc signupBloc = SignupBloc(userServices: userServices);
+  MockUserServices mockUserServices;
+  // MockUserModel mockUserModel;
+  SignupBloc? signupBloc;
 
   setUpAll(() async {
     setupCloudFirestoreMocks();
-    WidgetsFlutterBinding.ensureInitialized();
-    await Firebase.initializeApp();
+  });
+
+  setUp(() async {
+    mockUserServices = MockUserServices();
+    signupBloc = SignupBloc(userServices: mockUserServices);
   });
 
   tearDown(() {
-    signupBloc.close();
+    signupBloc?.close();
   });
 
   blocTest('emits [] when no events is called',
       build: () {
-        userServices = MockUserServices();
-        return SignupBloc(userServices: userServices);
+        mockUserServices = MockUserServices();
+        return SignupBloc(userServices: mockUserServices);
       },
       expect: () => []);
 
   blocTest(
       'emits [SignupInProgress] then [SignupSuccess] when [SignupRequested] is called',
       build: () {
-        userServices = MockUserServices();
-        return SignupBloc(userServices: userServices);
+        mockUserServices = MockUserServices();
+        return SignupBloc(userServices: mockUserServices);
       },
-      act: (SignupBloc bloc) => bloc.add(SignupRequested(
-          userModel:
-              UserModel(fullName: 'c', email: 'c@gmail.com', password: '789'))),
+      act: (SignupBloc bloc) {
+        return bloc.add(SignupRequested(
+            userModel: UserModel(
+                fullName: 'valid', email: 'valid', password: 'valid')));
+      },
       expect: () => [
             SignupInProgress(),
             SignupSuccess(),
@@ -51,16 +69,32 @@ void main() {
   blocTest(
       'emits [SignupFailure] when [SignupRequested] is called and service throws error',
       build: () {
-        userServices = MockUserServices();
-        when(userServices.signUp('fullName', 'email', 'password'))
-            .thenThrow(Exception());
-        return SignupBloc(userServices: userServices);
+        mockUserServices = MockUserServices();
+        return SignupBloc(userServices: mockUserServices);
       },
-      act: (SignupBloc bloc) => bloc.add(
-            SignupRequested(userModel: UserModel()),
-          ),
+      act: (SignupBloc bloc) => bloc.add(SignupRequested(
+          userModel:
+              UserModel(fullName: '123', email: '123', password: '123'))),
       expect: () => [
             SignupInProgress(),
-            SignupFailure(failErrorMessage: Exception().toString()),
+            SignupFailure(),
+          ]);
+
+  blocTest(
+      'emits [SignupFailure] with all error messages when [SignupErrorDisplayed] is called',
+      build: () {
+        mockUserServices = MockUserServices();
+        return SignupBloc(userServices: mockUserServices);
+      },
+      act: (SignupBloc bloc) => bloc.add(SignupErrorDetected(
+          fullNameErrorMessage: 'error',
+          emailErrorMessage: 'error',
+          passwordErrorMessage: 'error')),
+      expect: () => [
+            SignupFailure(
+              fullNameErrorMessage: 'error',
+              emailErrorMessage: 'error',
+              passwordErrorMessage: 'error',
+            ),
           ]);
 }
