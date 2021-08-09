8d03eac81ce27a9a109e4c904e6dfcd46b0f6b1f
khanh000huynh <47114045+khanh000huynh@users.noreply.github.com>
2021-08-09 17:33:26 +0700

    Revert "US03: Signup"

--- test/widget_tests/widgets/login_and_signup/password_text_form_field_test.dart
+++
@@ -1,7 +0,0 @@
-void main() {
-  // testWidgets('Should render correct label text', callback)
-
-  // testWidgets('Should render invalid password error message when password field is invalid', callback)
-
-  // testWidgets('Should render no error message when password field is valid', callback)
-}
