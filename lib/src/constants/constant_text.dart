c72e9059a3e4eaf8183674ea84ac84a5ebf58b1a
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-08-09 20:02:09 +0700

    Removed regex

--- lib/src/constants/constant_text.dart
+++ lib/src/constants/constant_text.dart
@@ -39,11 +39,6 @@ class SignupScreenText {
   static const String loginHere = 'Login Here';
 
   static const String signupFailedErrorText = 'Signup failed';
-
-  static RegExp emailRegex = RegExp(
-      r'''(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])''');
-  static RegExp passwordRegex =
-      RegExp(r'^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$');
 }
 
 class OnboardingTabletText {
