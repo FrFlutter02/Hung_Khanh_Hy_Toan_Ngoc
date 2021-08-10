ebc0a21b2b4a9f2e1c852f97cac7f03713f697fe
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-08-10 10:15:43 +0700

    custombutton

--- lib/src/widgets/custom_button.dart
+++ lib/src/widgets/custom_button.dart
@@ -1,22 +1,15 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import '../blocs/login_bloc/login_bloc.dart';
-import '../blocs/login_bloc/login_state.dart';
 
 import '../constants/constant_colors.dart';
-import '../utils/screen_util.dart';
 
-class CustomButton extends StatefulWidget {
-  final LoginBloc? loginBloc;
+class CustomButton extends StatelessWidget {
   final double width;
   final double height;
   final String value;
   final void Function() buttonOnPress;
 
   const CustomButton(
-      {required this.loginBloc,
-      required this.width,
+      {required this.width,
       required this.height,
       required this.value,
       required this.buttonOnPress,
@@ -24,34 +17,18 @@ class CustomButton extends StatefulWidget {
       : super(key: key);
 
   @override
-  _CustomButtonState createState() => _CustomButtonState();
-}
-
-class _CustomButtonState extends State<CustomButton> {
-  StreamSubscription? loginStreamSubscription;
-  bool isLoading = false;
-  @override
   Widget build(BuildContext context) {
-    final ScreenUtil _screenUtil = ScreenUtil();
     return Container(
-      width: widget.width,
-      height: widget.height,
+      width: width,
+      height: height,
       child: ElevatedButton(
-        onPressed: !isLoading ? widget.buttonOnPress : null,
-        child: isLoading
-            ? SizedBox(
-                child: CircularProgressIndicator(
-                    valueColor: AlwaysStoppedAnimation<Color>(AppColor.white)),
-                width: _screenUtil.width(25),
-                height: _screenUtil.height(25),
-              )
-            : Text(
-                widget.value,
-                style: Theme.of(context)
-                    .textTheme
-                    .bodyText2!
-                    .copyWith(color: AppColor.white),
+        onPressed: buttonOnPress,
+        child: Text(
+          value,
+          style: Theme.of(context).textTheme.bodyText2!.copyWith(
+                color: AppColor.white,
               ),
+        ),
         style: ButtonStyle(
           backgroundColor: MaterialStateProperty.all(AppColor.green),
           foregroundColor: MaterialStateProperty.all(AppColor.white),
@@ -64,25 +41,5 @@ class _CustomButtonState extends State<CustomButton> {
         ),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    loginStreamSubscription?.cancel();
-    super.dispose();
-  }
-
-  @override
-  void initState() {
-    loginStreamSubscription = widget.loginBloc?.stream.listen((loginState) {
-      setState(() {
-        if (loginState is LoginInProgress) {
-          isLoading = true;
-        } else {
-          isLoading = false;
-        }
-      });
-    });
-    super.initState();
   }
 }
