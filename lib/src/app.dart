c06db491b3a7784308ffd92c494f13ba24864fa1
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-08-12 21:17:08 +0700

    Added flutter_screenutil

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_device_type/flutter_device_type.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../src/blocs/forgot_password_bloc/forgot_password_bloc.dart';
 import '../src/blocs/login_bloc/login_bloc.dart';
@@ -14,15 +16,21 @@ import '../src/services/user_services.dart';
 // import 'screens/onboarding_screen.dart';
 
 class App extends StatelessWidget {
-  final userServices = UserServices();
-
   @override
   Widget build(BuildContext context) {
+    final userServices = UserServices();
+    Size designSize = Size(375, 812);
+
+    if (Device.get().isTablet) {
+      designSize = Size(768, 1024);
+    }
+
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
       statusBarBrightness: Brightness.dark,
       statusBarIconBrightness: Brightness.dark,
     ));
+
     return MultiBlocProvider(
       providers: [
         BlocProvider(
@@ -33,18 +41,21 @@ class App extends StatelessWidget {
         BlocProvider(
             create: (context) => SignupBloc(userServices: userServices)),
       ],
-      child: MaterialApp(
-        theme: ThemeData(fontFamily: "Nunito-Regular"),
-        debugShowCheckedModeBanner: false,
-        routes: {
-          "/": (context) => Searchscreen()
-          // "/": (context) => OnboardingScreen(),
-          // "/forgotPasswordScreen": (context) => ForgotPasswordScreen(),
-          // "/homeScreen": (context) => HomeScreen(),
-          // "/loginScreen": (context) => LoginScreen(),
-          // "/onboardingScreen": (context) => OnboardingScreen(),
-          // "/signupScreen": (context) => SignupScreen(),
-        },
+      child: ScreenUtilInit(
+        designSize: designSize,
+        builder: () => MaterialApp(
+          theme: ThemeData(fontFamily: "Nunito-Regular"),
+          debugShowCheckedModeBanner: false,
+          routes: {
+            "/": (context) => Searchscreen()
+            // "/": (context) => OnboardingScreen(),
+            // "/forgotPasswordScreen": (context) => ForgotPasswordScreen(),
+            // "/homeScreen": (context) => HomeScreen(),
+            // "/loginScreen": (context) => LoginScreen(),
+            // "/onboardingScreen": (context) => OnboardingScreen(),
+            // "/signupScreen": (context) => SignupScreen(),
+          },
+        ),
       ),
     );
   }
