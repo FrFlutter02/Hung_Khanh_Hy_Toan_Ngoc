acb4b3169396af2ed15963c6cedd10590e915f8a
giahy1 <ngiahyiu@gmail.com>
2021-08-13 15:33:45 +0700

    code coverge 84%

--- lib/src/constants/constant_colors.dart
+++ lib/src/constants/constant_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColor {
   static const green = Color(0xff30BE76);
-  static const iconText = Color(0xff363837);
+  static const iconGrey = Color(0xff363837);
   static const primaryBlack = Color(0xff030F09);
   static const primaryGrey = Color(0xff606060);
   static const secondaryGrey = Color(0xffA8A8A8);
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/src/constants/constant_colors.dart';
+import 'package:mobile_app/src/screens/recipe_screen.dart';
+import 'package:mobile_app/src/screens/search_screen.dart';
+import 'package:mobile_app/src/screens/user_profile_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+/// This is the private State class that goes with HomeScreen.
+class _HomeScreenState extends State<HomeScreen> {
+  int _selectedIndex = 1;
+  static const List<Widget> _widgetOptions = <Widget>[
+    SearchScreen(),
+    RecipeScreen(),
+    UserProfileScreen()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage('assets/images/search_icon.jpg'),
+                color: _selectedIndex == 0 ? AppColor.green : AppColor.iconGrey,
+              ),
+              label: 'Search'),
+          BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage('assets/images/carosel_icon.jpg'),
+                color: _selectedIndex == 1 ? AppColor.green : AppColor.iconGrey,
+              ),
+              label: 'Recipe'),
+          BottomNavigationBarItem(
+              icon: Image(
+                image: AssetImage('assets/images/user_profile_icon.jpg'),
+                color: _selectedIndex == 2 ? AppColor.green : AppColor.iconGrey,
+              ),
+              label: 'User profile')
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
