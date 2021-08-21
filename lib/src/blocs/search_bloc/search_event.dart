ac58f7eba6636265296298dab3e97e858b4aef9d
Huỳnh Ngọc Khánh <khanh000huynh@gmail.com>
2021-08-21 19:08:04 +0700

    Refactor search_event

--- lib/src/blocs/search_bloc/search_event.dart
+++ lib/src/blocs/search_bloc/search_event.dart
@@ -2,25 +2,31 @@ import 'package:equatable/equatable.dart';
 
 abstract class SearchEvent extends Equatable {
   const SearchEvent();
-
-  @override
-  List<Object> get props => [];
 }
 
 class SearchTextFieldChanged extends SearchEvent {
   final String recipeTextFieldValue;
 
   const SearchTextFieldChanged({required this.recipeTextFieldValue});
+
+  @override
+  List<Object> get props => [recipeTextFieldValue];
 }
 
 class SearchRecipeRequested extends SearchEvent {
   final String searchQuery;
 
   const SearchRecipeRequested({required this.searchQuery});
+
+  @override
+  List<Object> get props => [searchQuery];
 }
 
 class SearchAutofilled extends SearchEvent {
   final String autofillValue;
 
   const SearchAutofilled({required this.autofillValue});
+
+  @override
+  List<Object> get props => [autofillValue];
 }
