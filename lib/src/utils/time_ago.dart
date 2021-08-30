b4b54dd1c7336180e1fb68de97429b5b94457482
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-08-30 13:41:05 +0700

    fix time ago

--- lib/src/utils/time_ago.dart
+++ lib/src/utils/time_ago.dart
@@ -1,24 +1,22 @@
+import 'package:intl/intl.dart';
+
 class TimeAgo {
   static String timeAgoSinceDate(int dateString) {
     final DateTime dateNow = DateTime.now();
+    final String format = DateFormat('dd-MM')
+        .format(DateTime.fromMillisecondsSinceEpoch((dateString)));
     final difference =
         dateNow.difference(DateTime.fromMillisecondsSinceEpoch(dateString));
-    if (difference.inDays > 8) {
-      return dateString.toString();
+    if (difference.inDays >= 7) {
+      return format;
     } else if ((difference.inDays / 7).floor() >= 1) {
       return '1 week ago';
-    } else if (difference.inDays >= 2) {
-      return '${difference.inDays} days ago';
     } else if (difference.inDays >= 1) {
-      return '1 day ago';
-    } else if (difference.inHours >= 2) {
-      return '${difference.inHours}h ago';
+      return '${difference.inDays}d ago';
     } else if (difference.inHours >= 1) {
-      return '1h ago';
-    } else if (difference.inMinutes >= 2) {
-      return '${difference.inMinutes}m ago';
+      return '${difference.inHours}h ago';
     } else if (difference.inMinutes >= 1) {
-      return '1m ago';
+      return '${difference.inMinutes}m ago';
     } else if (difference.inSeconds >= 3) {
       return '${difference.inSeconds}s ago';
     } else {
--- lib/src/widgets/recipe_feed/card_header.dart
+++ lib/src/widgets/recipe_feed/card_header.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import '../../utils/time_ago.dart';
 import '../../constants/constant_colors.dart';
 
