436d11dc2996b74a34a63d20c7786ed91bf14b2b
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-08-30 20:37:30 +0700

    time ago

--- lib/src/utils/time_ago.dart
+++ lib/src/utils/time_ago.dart
@@ -1,24 +1,24 @@
-import 'package:intl/intl.dart';
-
 class TimeAgo {
   static String timeAgoSinceDate(int dateString) {
     final DateTime dateNow = DateTime.now();
-    final String format = DateFormat('dd-MM')
-        .format(DateTime.fromMillisecondsSinceEpoch((dateString)));
     final difference =
         dateNow.difference(DateTime.fromMillisecondsSinceEpoch(dateString));
-    if (difference.inDays >= 7) {
-      return format;
+
+    if (difference.inDays >= 365) {
+      print(difference.inDays);
+      return '${(difference.inDays / 365).floor()} year ago';
+    } else if (difference.inDays >= 30) {
+      return '${(difference.inDays / 30).floor()} month ago';
     } else if ((difference.inDays / 7).floor() >= 1) {
-      return '1 week ago';
+      return '${(difference.inDays / 7).floor()} week ago';
     } else if (difference.inDays >= 1) {
-      return '${difference.inDays}d ago';
+      return '${difference.inDays} day ago';
     } else if (difference.inHours >= 1) {
-      return '${difference.inHours}h ago';
+      return '${difference.inHours} hour ago';
     } else if (difference.inMinutes >= 1) {
-      return '${difference.inMinutes}m ago';
+      return '${difference.inMinutes} minute ago';
     } else if (difference.inSeconds >= 3) {
-      return '${difference.inSeconds}s ago';
+      return '${difference.inSeconds} second ago';
     } else {
       return 'Just now';
     }
