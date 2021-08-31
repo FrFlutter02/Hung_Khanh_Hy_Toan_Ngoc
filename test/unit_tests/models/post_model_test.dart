2f96603cbadde84539389458e5135837a8c51ca7
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-08-31 12:02:07 +0700

    test model

--- test/unit_tests/models/post_model_test.dart
+++ test/unit_tests/models/post_model_test.dart
@@ -1,24 +1,30 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:mobile_app/src/models/post_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile_app/src/models/post_model.dart';
 
-// void main() {
-//   test('Should return Post Model from json', () {
-//      Post  post =    Post.fromSnapshot(  'recipeId': snapshot['recipeId'],
-//       'userId': snapshot['userId'],
-//       'description': snapshot['description'],
-//       'likes': snapshot['likes'],
-//       'comments': snapshot['comments'],
-//       'name': snapshot['name'],
-//       'backgroundImage': snapshot['backgroundImage'],
-//       'time': snapshot['time'],);
-  
-//     expect(post, Post(      recipeId: 'recipeId',
-//       userId: 'userId',
-//       description: 'description',
-//       likes: 0,
-//       comments:0,
-//       name: 'name',
-//       backgroundImage: 'backgroundImage',
-//       time:  0,));
-//   });
-// }
+void main() {
+  test('Should return Post Model from json', () {
+    final Post post = Post(
+      recipeId: 'recipeId',
+      userId: 'userId',
+      description: 'description',
+      likes: 0,
+      comments: 0,
+      name: 'name',
+      backgroundImage: 'backgroundImage',
+      time: 0,
+    );
+
+    expect(
+        post,
+        Post(
+          recipeId: 'recipeId',
+          userId: 'userId',
+          description: 'description',
+          likes: 0,
+          comments: 0,
+          name: 'name',
+          backgroundImage: 'backgroundImage',
+          time: 0,
+        ));
+  });
+}
