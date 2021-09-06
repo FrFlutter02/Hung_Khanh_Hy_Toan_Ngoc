1991ce93d015ee5f6ac620c970fb11a2b85ec4e1
TanHung <66214035+tanhung43@users.noreply.github.com>
2021-09-06 13:52:16 +0700

    edit part

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,24 +4,24 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_device_type/flutter_device_type.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-import 'screens/navigation_screen.dart';
-import 'services/create_recipe_services.dart';
-import 'blocs/new_recipe_bloc/new_recipe_bloc.dart';
-import 'screens/new_recipe_screen.dart';
 import 'blocs/forgot_password_bloc/forgot_password_bloc.dart';
 import 'blocs/keyword_search_bloc/keyword_search_bloc.dart';
 import 'blocs/login_bloc/login_bloc.dart';
-import 'blocs/signup_bloc/signup_bloc.dart';
+import 'blocs/new_recipe_bloc/new_recipe_bloc.dart';
 import 'blocs/post_bloc/post_bloc.dart';
+import 'blocs/signup_bloc/signup_bloc.dart';
 import 'screens/forgot_password_screen.dart';
-import 'screens/signup_screen.dart';
-import 'services/search_services.dart';
-import 'services/user_services.dart';
 import 'screens/login_screen.dart';
+import 'screens/navigation_screen.dart';
+import 'screens/new_recipe_screen.dart';
 import 'screens/onboarding_screen.dart';
 import 'screens/recipe_feed_screen.dart';
 import 'screens/search_screen.dart';
+import 'screens/signup_screen.dart';
+import 'services/create_recipe_services.dart';
 import 'services/post_service.dart';
+import 'services/search_services.dart';
+import 'services/user_services.dart';
 
 class App extends StatelessWidget {
   @override
